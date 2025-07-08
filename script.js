a8a50e416bbd70d5c942d37981f6d8ecb6184191
SherinNITW <sherinjosepoonjar@gmail.com>
2025-07-08 12:18:01 +0400

    Scrollable

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-const products = document.querySelectorAll(".imgs");
-for(const item of products){
-  item.addEventListener("wheel",(e)=>{
-    e.preventDefault();
-    item.scrollLeft += e.deltaY;
-  });
+const items = document.querySelectorAll(".imgs");
+
+for (const item of items) {
+  setInterval(() => {
+    item.scrollLeft += 1; // scroll 1 pixel every 30ms
+    if (item.scrollLeft + item.clientWidth >= item.scrollWidth) {
+      item.scrollLeft = 0; // go back to start
+    }
+  }, 30); // scroll every 30 milliseconds
 }
